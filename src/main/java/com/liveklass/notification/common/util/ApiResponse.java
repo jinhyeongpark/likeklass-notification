27276065861fb1dc9da820387e827ac9c460f958
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-04-03 05:03:42 +0900

    feat: api 응답에 대해 code와 message 커스텀을 위한 메서드 추가

--- src/main/java/com/liveklass/notification/common/util/ApiResponse.java
+++ src/main/java/com/liveklass/notification/common/util/ApiResponse.java
@@ -5,13 +5,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
-@JsonPropertyOrder({"success", "code", "message", "result"}) // 필드명 변경 고려
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
 public class ApiResponse<T> {
 
-    @JsonProperty("isSuccess") // JSON에서도 명확하게 isSuccess로 나오도록 강제
+    @JsonProperty("isSuccess")
     private final Boolean isSuccess;
     private final String code;
     private final String message;
@@ -20,6 +21,10 @@ public class ApiResponse<T> {
     private final T result;
 
     // --- 성공 케이스 ---
+    public static <T> ApiResponse<T> onSuccess(HttpStatus status, String code, String message, T result) {
+        return new ApiResponse<>(true, code, message, result);
+    }
+
 
     public static <T> ApiResponse<T> onSuccess(T result) {
         return new ApiResponse<>(true, "COMMON200", "요청에 성공했습니다.", result);
@@ -29,7 +34,6 @@ public class ApiResponse<T> {
         return new ApiResponse<>(true, "COMMON201", "성공적으로 생성되었습니다.", result);
     }
 
-    // 데이터 없이 성공 메시지만 보낼 때 (204 대응)
     public static <T> ApiResponse<T> onSuccess() {
         return new ApiResponse<>(true, "COMMON200", "성공적으로 처리되었습니다.", null);
     }
