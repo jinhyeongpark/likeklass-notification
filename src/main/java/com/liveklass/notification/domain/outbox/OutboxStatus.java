81055d5a38f38f27e85ddc426f67b11986887b1d
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-04-03 05:05:41 +0900

    feat: 알림 타입 및 Outbox 상태 관련 Enum 정의

--- src/main/java/com/liveklass/notification/domain/outbox/OutboxStatus.java
+++ src/main/java/com/liveklass/notification/domain/outbox/OutboxStatus.java
@@ -1,4 +1,15 @@
 package com.liveklass.notification.domain.outbox;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum OutboxStatus {
+    INIT("발행 대기"),
+    PROCESSING("처리 중"), // 다중 인스턴스 환경에서 중복 발송 방지를 위한 상태
+    COMPLETED("전송 완료"),
+    FAILED("최종 실패");
+
+    private final String description;
 }
