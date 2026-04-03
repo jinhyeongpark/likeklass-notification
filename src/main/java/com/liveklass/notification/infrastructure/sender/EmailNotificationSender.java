6022ab4be8140373502d9af190f20719c9d4bd34
jinhyeongpark <156793718+jinhyeongpark@users.noreply.github.com>
2026-04-03 23:52:59 +0900

    feat: Sender 구현체 수정
     - 기존: LogNotificationSender
     - 변경 후: InApp과 Email을 로그 내용만 다르게 분리하여 구현

---
+++ src/main/java/com/liveklass/notification/infrastructure/sender/EmailNotificationSender.java
@@ -0,0 +1,27 @@
+package com.liveklass.notification.infrastructure.sender;
+
+import com.liveklass.notification.domain.notification.Notification;
+import com.liveklass.notification.domain.notification.NotificationChannel;
+import com.liveklass.notification.domain.notification.NotificationSender;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class EmailNotificationSender implements NotificationSender {
+
+    @Override
+    public void send(Notification notification) {
+        // 실제 운영 환경에서는 SMTP / SES 등 이메일 발송 로직으로 교체
+        log.info("[EMAIL] 이메일 알림 발송");
+        log.info("   > 수신자 ID: {}", notification.getReceiverId());
+        log.info("   > 제목: {}", notification.getTitle());
+        log.info("   > 내용: {}", notification.getContent());
+        log.info("   > 데이터: {}", notification.getReferenceData());
+    }
+
+    @Override
+    public NotificationChannel getChannel() {
+        return NotificationChannel.EMAIL;
+    }
+}
--- src/main/java/com/liveklass/notification/infrastructure/sender/InAppNotificationSender.java
+++ src/main/java/com/liveklass/notification/infrastructure/sender/InAppNotificationSender.java
@@ -1,20 +1,27 @@
 package com.liveklass.notification.infrastructure.sender;
 
 import com.liveklass.notification.domain.notification.Notification;
+import com.liveklass.notification.domain.notification.NotificationChannel;
 import com.liveklass.notification.domain.notification.NotificationSender;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-public class LogNotificationSender implements NotificationSender {
+public class InAppNotificationSender implements NotificationSender {
 
     @Override
     public void send(Notification notification) {
-        log.info("[REAL-TIME LOG] 알림 발송 성공");
+        // 실제 운영 환경에서는 FCM / WebSocket 등 인앱 푸시 로직으로 교체
+        log.info("[IN_APP] 인앱 알림 발송");
         log.info("   > 수신자 ID: {}", notification.getReceiverId());
         log.info("   > 제목: {}", notification.getTitle());
         log.info("   > 내용: {}", notification.getContent());
         log.info("   > 데이터: {}", notification.getReferenceData());
+    }
+
+    @Override
+    public NotificationChannel getChannel() {
+        return NotificationChannel.IN_APP;
     }
 }
